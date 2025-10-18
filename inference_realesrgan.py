aa36e3907bad1584a76a98b17f388eb6708247cd
Stefano Raneri <96783398+st1vms@users.noreply.github.com>
2025-10-18 16:33:53 +0200

    Skip non-image files during processing
    Add check for None image to skip non-image files.

--- inference_realesrgan.py
+++ inference_realesrgan.py
@@ -135,6 +135,10 @@ def main():
         print('Testing', idx, imgname)
 
         img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        if img is None:
+            print(f"Skipping file '{imgname}' as it's not an image...")
+            continue
+
         if len(img.shape) == 3 and img.shape[2] == 4:
             img_mode = 'RGBA'
         else:
